8b05664b2e4875f911b1c89e5653c02d6b2808d7
Christian Paleologou <Chrispaleologou@gmail.com>
2026-01-27 21:30:25 -0800

    Quartz sync: Jan 27, 2026, 9:30 PM

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -3,15 +3,6 @@ import { classNames } from "../util/lang"
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
-  const hiddenSlugs = ["index"]
-
-  // Fix: Cast fileData.slug to 'string' to satisfy TypeScript
-  if (hiddenSlugs.includes(fileData.slug as string)) {
-    return null
-  }
-
-  // You can remove the old "index" check here because it's included in the list above!
-
   if (title) {
     return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
   } else {
@@ -24,4 +15,5 @@ ArticleTitle.css = `
   margin: 2rem 0 0 0;
 }
 `
+
 export default (() => ArticleTitle) satisfies QuartzComponentConstructor
