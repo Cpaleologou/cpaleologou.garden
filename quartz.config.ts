43d75127a512b614c07e5e22fd044ecd1e635949
Christian Paleologou <Chrispaleologou@gmail.com>
2026-01-29 12:00:00 -0800

    Quartz sync: Jan 29, 2026, 12:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "CP",
+    pageTitle: "ChristianP.space",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
