01f39bcf292c6343080f1c90576ee9e5f71ae6c6
Christian Paleologou <Chrispaleologou@gmail.com>
2026-01-29 17:58:30 -0800

    Quartz sync: Jan 29, 2026, 5:58 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -99,7 +99,6 @@ export default (() => {
         })}
 
         <link href="https://fonts.googleapis.com/css2?family=Pinyon+Script&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&display=swap" rel="stylesheet" />
       </head>
     )
   }
