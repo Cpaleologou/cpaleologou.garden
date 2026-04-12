0fac23fbfb2c3cf6fdd1d791fa47ade581d33d26
Christian Paleologou <Chrispaleologou@gmail.com>
2026-04-12 11:49:05 -0700

    Quartz sync: Apr 12, 2026, 11:49 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -91,18 +91,9 @@ export const defaultContentPageLayout: PageLayout = {
     ),
   ],
   right: [
-    Component.ConditionalRender({
-      component: Component.Graph(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.ConditionalRender({
-      component: Component.DesktopOnly(Component.TableOfContents()),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.ConditionalRender({
-      component: Component.Backlinks(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
+    Component.Graph(),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
   ],
 }
 
